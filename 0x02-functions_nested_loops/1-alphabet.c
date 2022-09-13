149aaa97509d9bba0e760b50ad97aa3db451ba7e
Eddy-Mark <111184013+Eddy-mark@users.noreply.github.com>
2022-09-13 09:26:48 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
-#include "stdio.h"
+#include "holberton.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase.
+ * main - Prints Holberton as a message.
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+int main(void)
 {
-	int i;
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
 
-	for (i = 97; i <= 122; i++)
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
-		_putchar(i);
+		_putchar(str[count]);
 	}
 	_putchar('\n');
+	return (0);
 }
